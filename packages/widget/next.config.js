a88ba96e3d953f4b196c4b631630332e525148ab
steven94kr <rlgns98kr@gmail.com>
2026-02-19 16:42:33 +0900

    clarify env name

--- packages/widget/next.config.js
+++ packages/widget/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   publicRuntimeConfig: {
-    gatewayApiUrl: process.env.GATEWAY_API_URL || 'http://localhost:3001',
+    gatewayApiUrl: process.env.GATEWAY_PUBLIC_URL || 'http://localhost:3001',
     faucetApiUrl: process.env.FAUCET_API_URL || 'http://localhost:3003',
   },
   webpack: (config) => {
