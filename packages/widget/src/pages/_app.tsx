3da8c459516f3f0e6d0e158175bf2393a351508c
Noeurn Neang <noeurnnneang@gmail.com>
2026-02-26 13:20:01 +0700

    [ON-173] Update wallet connection flow to always show wallet selection screen

--- packages/widget/src/pages/_app.tsx
+++ packages/widget/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
@@ -9,11 +9,6 @@ import { config } from '../wagmi';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [client] = useState(() => new QueryClient());
-
-  useEffect(() => {
-    localStorage.removeItem('wagmi.recentConnectorId');
-    localStorage.removeItem('wagmi.store');
-  }, []);
 
   return (
     <WagmiProvider config={config} reconnectOnMount={false}>
